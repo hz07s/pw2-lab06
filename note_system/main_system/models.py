7ae6f3dde6bde9c194e13350d2c55ffd9b173f3d
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-05-27 17:00:01 -0500

    Agregando la clase Students dentro del archivo models.py

--- note_system/main_system/models.py
+++ note_system/main_system/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    LEVEL_CHOICES = [
+        ('IN', 'Initial'),
+        ('PR', 'Primary'),
+        ('SE', 'Secondary')
+    ]
+
+    name = models.CharField(max_length=50, blank=False)
+    surname = models.CharField(max_length=50, blank=False)
+    age = models.IntegerField(blank=False)
+    grade = models.IntegerField(blank=False)
+    active = models.BooleanField(default=True, blank=False)
+    level = models.CharField(max_length=2, choices=LEVEL_CHOICES, blank=False)
+
+    def __str__(self):
+        return f'{self.name} ({self.grade})'
+    class Meta:
+        ordering = ['name']
+    

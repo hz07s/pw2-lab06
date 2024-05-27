279bf20ee325914cb447123594331191d88876ed
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-05-27 18:00:00 -0500

    Agregando el metodo insertStudent a views.py para agregar las vistas del formulario de estudiantes

--- note_system/main_system/views.py
+++ note_system/main_system/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Student
+from .forms import InsertStudentForm
 # Create your views here.
+def insertStudent(request):
+    form = InsertStudentForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = InsertStudentForm()
+        
+    context = {
+        'form': form
+        }
+    return render(request, 'insertStudent.html', context)

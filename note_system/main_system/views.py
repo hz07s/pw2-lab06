83a0bf84b69b502238bdd5c937bd8c4ce9dce12a
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-28 18:49:00 -0500

    En viems.py se creo la funcion insertCouse() que mostrara el formulario en el html, guardara y registrara los datos ingresados por el usuario

--- note_system/main_system/views.py
+++ note_system/main_system/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import InsertCourseForm
 
 # Create your views here.
+def insertCourse(request):
+    form = InsertCourseForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = InsertCourseForm()
+        
+    context = {
+        'form': form
+        }
+    return render(request, 'insertCourse.html', context)

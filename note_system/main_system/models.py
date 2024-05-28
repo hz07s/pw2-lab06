664f4063c70789bce5bcea5b4fc21588f59865d9
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-27 22:37:00 -0500

    Creando la clase Course en models.py para registrar los cursos existentes

--- note_system/main_system/models.py
+++ note_system/main_system/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    credits = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
